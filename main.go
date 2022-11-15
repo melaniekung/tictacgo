e3f72ce8f448533365ab0f263ea636a54f814873
melaniekung <melaniekung@live.com>
2022-11-15 15:02:57 -0800

    Alter application to work with html and css

--- main.go
+++ main.go
@@ -1,147 +1,161 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
 )
 
-var attempts = 0
-var board [9]string
+var tmpl *template.Template
 var player = 1
+var data Game
+
+type Move struct {
+	Player int
+	Box    int
+}
+
+type Game struct {
+	Note  string
+	Board [9]Move
+}
 
 func main() {
-	fmt.Println("Welcome to mel's tic-tac-toe game!")
+	// create empty servemux
+	mux := http.NewServeMux()
 
-	// set board
+	// serve static index.html
+	fsHtml := http.FileServer(http.Dir("templates"))
+	mux.Handle("/", fsHtml)
+
+	// call startGame function
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.HandleFunc("/start", startGame)
+
+	// call nextMove function
+	next := http.HandlerFunc(nextMove)
 	for i := 0; i < 9; i++ {
-		board[i] = " "
+		url := "/" + strconv.Itoa(i)
+		mux.Handle(url, next)
 	}
 
-	nextMove(player)
+	// start server
+	log.Println("Tic-Tac-Go on localhost:3000")
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-// ask player for move
-func nextMove(player int) {
-	var move int
+func startGame(w http.ResponseWriter, r *http.Request) {
+	// reset player
+	player = 1
 
-	// set attempts back to 0
-	attempts = 0
+	// set template
+	tmpl = template.Must(template.ParseFiles("templates/start.html"))
 
-	fmt.Println("----------")
-	fmt.Println("Player", player, "'s turn:")
-	fmt.Scan(&move)
+	// reset and display board
+	data = Game{
+		Note: "Player 1's turn:",
+		Board: [9]Move{
+			{Player: 0, Box: 0},
+			{Player: 0, Box: 1},
+			{Player: 0, Box: 2},
+			{Player: 0, Box: 3},
+			{Player: 0, Box: 4},
+			{Player: 0, Box: 5},
+			{Player: 0, Box: 6},
+			{Player: 0, Box: 7},
+			{Player: 0, Box: 8},
+		},
+	}
+
+	tmpl.Execute(w, data)
+}
+
+func nextMove(w http.ResponseWriter, r *http.Request) {
+	// get move number
+	m := r.URL.String()[1:]
+	move, err := strconv.Atoi(m)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// check that the move made is valid
-	validateMove(move)
-}
+	valid := validateMove(move)
 
-func validateMove(move int) {
-	valid := false
-
-	// check if move is a valid number
-	for i := 0; i < 9; i++ {
-		if move == i {
-			valid = true
-			break
-		}
-	}
-
-	// check if move is already played
-	if board[move] == " " {
-		valid = true
-		fmt.Println("")
-	} else {
-		valid = false
-	}
+	var note string
 
 	if valid {
-		// add move to board
-		addMove(move)
-	} else {
-		// give player 3 attempts to make a valid move
-		if attempts <= 3 {
-			attempts = attempts + 1
-			fmt.Print("Invalid move, please select another move (", attempts, " left): ")
-			fmt.Scan(&move)
+		// store move on board
+		data.Board[move].Player = player
 
-			validateMove(move)
-		} else {
-			fmt.Println("Sorry, out of attempts")
-		}
-	}
-}
+		gameover, full := checkBoard()
 
-// add X for player 1 and O for player 2 to the board
-func addMove(move int) {
-	// store move on board
-	if player == 1 {
-		board[move] = "X"
-	} else {
-		board[move] = "O"
-	}
-
-	// display board
-	for i := 0; i < 9; i++ {
-		if i == 2 || i == 5 || i == 8 {
-			fmt.Println(board[i])
-			if i != 8 {
-				fmt.Println("- - -")
+		if !gameover {
+			if player == 1 {
+				player = 2
+			} else {
+				player = 1
 			}
+			note = "Player " + strconv.Itoa(player) + "'s Turn:"
 		} else {
-			fmt.Print(board[i], "|")
+			if full && gameover {
+				note = "GAME OVER! Player " + strconv.Itoa(player) + " Wins!"
+			} else if full {
+				note = "Game over... Out of moves!"
+			} else {
+				note = "GAME OVER! Player " + strconv.Itoa(player) + " Wins!"
+			}
 		}
+
+		// pass player number to html
+		data.Note = note
+	} else {
+		data.Note = "Move already taken, try again: "
 	}
 
-	checkBoard()
+	tmpl := template.Must(template.ParseFiles("./templates/start.html"))
+	tmpl.Execute(w, data)
 }
 
-func checkBoard() {
+func validateMove(move int) bool {
+	// check if move is valid
+	if data.Board[move].Player == 0 {
+		return true
+	}
+
+	return false
+}
+
+func checkBoard() (bool, bool) {
 	gameover := false
 
-	// check if there is a horizontal win
-	check1 := strings.Join(board[0:4], "")
-	check2 := strings.Join(board[4:7], "")
-	check3 := strings.Join(board[7:9], "")
-
-	if check1 == "XXX" || check1 == "OOO" ||
-		check2 == "XXX" || check2 == "OOO" ||
-		check3 == "XXX" || check3 == "OOO" {
-		fmt.Println("Game over!")
-		fmt.Println("Player", player, "wins!")
-		gameover = true
-	}
-
-	// check if there is a vertical win
-	check4 := board[0] + board[3] + board[6]
-	check5 := board[1] + board[4] + board[7]
-	check6 := board[2] + board[5] + board[8]
-
-	if check4 == "XXX" || check4 == "OOO" ||
-		check5 == "XXX" || check5 == "OOO" ||
-		check6 == "XXX" || check6 == "OOO" {
-		fmt.Println("Game over!")
-		fmt.Println("Player", player, "wins!")
-		gameover = true
-	}
-
-	// check if there is a diagonal win
-	check0 := board[2] + board[4] + board[6]
-	check7 := board[0] + board[4] + board[8]
-
-	if check0 == "XXX" || check0 == "OOO" ||
-		check7 == "XXX" || check7 == "OOO" {
-		fmt.Println("Game over!")
-		fmt.Println("Player", player, "wins!")
-		gameover = true
-	}
-
-	if !gameover {
-		if player == 1 {
-			player = 2
-		} else {
-			player = 1
+	// check if board is full
+	full := true
+	for i := 0; i < 9; i++ {
+		if data.Board[i].Player == 0 {
+			full = false
 		}
-
-		nextMove(player)
 	}
+
+	if full {
+		gameover = true
+	}
+
+	// check if there is a win (horizontal, vertical , diagonal)
+	if (data.Board[0].Player != 0 && data.Board[0].Player == data.Board[1].Player && data.Board[1].Player == data.Board[2].Player) ||
+		(data.Board[3].Player != 0 && data.Board[3].Player == data.Board[4].Player && data.Board[4].Player == data.Board[5].Player) ||
+		(data.Board[6].Player != 0 && data.Board[6].Player == data.Board[7].Player && data.Board[7].Player == data.Board[8].Player) ||
+		(data.Board[0].Player != 0 && data.Board[0].Player == data.Board[3].Player && data.Board[3].Player == data.Board[6].Player) ||
+		(data.Board[1].Player != 0 && data.Board[1].Player == data.Board[4].Player && data.Board[4].Player == data.Board[7].Player) ||
+		(data.Board[2].Player != 0 && data.Board[2].Player == data.Board[5].Player && data.Board[5].Player == data.Board[8].Player) ||
+		(data.Board[2].Player != 0 && data.Board[2].Player == data.Board[4].Player && data.Board[4].Player == data.Board[6].Player) ||
+		(data.Board[0].Player != 0 && data.Board[0].Player == data.Board[4].Player && data.Board[4].Player == data.Board[8].Player) {
+		gameover = true
+	}
+
+	return gameover, full
 }
